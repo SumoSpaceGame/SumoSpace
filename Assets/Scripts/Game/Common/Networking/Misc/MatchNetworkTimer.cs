d6fc671937c62203b6f1ea8f5667cadcf316fb1f
sorrer <sorrer@gmail.com>
2021-10-20 20:50:26 -0400

    Cleared events on destroy to make sure all connections are terminated.

--- Assets/Scripts/Game/Common/Networking/Misc/MatchNetworkTimer.cs
+++ Assets/Scripts/Game/Common/Networking/Misc/MatchNetworkTimer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4663c17123ec77ece3d3725283bbae6b032e9050fd6d97fc77f59a207e4dcf28
-size 8356
+oid sha256:754bd0f5a12124fea991bbdde19321df87e1c2a8e09a0eea9bb7b62ab5c74baf
+size 8667
