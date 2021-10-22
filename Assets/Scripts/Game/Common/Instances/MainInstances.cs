e599860d702abd8fddeda651df05d5fe5d7ecd59
sorrer <sorrer@gmail.com>
2021-10-22 18:56:10 -0400

    Fixed script loading order and input layer

--- Assets/Scripts/Game/Common/Instances/MainInstances.cs
+++ Assets/Scripts/Game/Common/Instances/MainInstances.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db7dff61a19f301b186c704dd4461829f8d051dcfd3b9d7acd6e55d6ba7e114a
-size 3909
+oid sha256:00271443b12c9594f9eebd92ba1427d5f5ba28e8cab46a229f87a9018ba5f228
+size 3907
--- Assets/Scripts/Game/Common/Networking/InputLayerNetworkManager.cs
+++ Assets/Scripts/Game/Common/Networking/InputLayerNetworkManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53ead2ebd84510d36794fc407557a6cdff9cf426eec2ea5e05170a2991449501
-size 2674
+oid sha256:896705696f7103a189f85d24656337bf3e241b8d1742ca3f7b4a07cecec5d77b
+size 2687
