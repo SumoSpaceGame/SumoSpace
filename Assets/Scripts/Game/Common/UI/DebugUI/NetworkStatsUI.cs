876642d0296a3beed12bb241af1a41860f48ce77
sorrer <sorrer@gmail.com>
2022-03-01 16:10:58 -0500

    Bandwidth properly read

--- Assets/Scripts/Game/Common/UI/DebugUI/NetworkStatsUI.cs
+++ Assets/Scripts/Game/Common/UI/DebugUI/NetworkStatsUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6d7056f644b0c07ae42bc80c609d80ed48acd329cfe4b2398d5f2c790c7c2c8
-size 3563
+oid sha256:c98c557abddf48e9e03e4cf3257bb7adeb73c3b7dba91fb87f1491dc30c3c526
+size 4102
