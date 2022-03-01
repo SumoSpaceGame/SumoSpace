8f8d410c9a50f4fe41fbf04018544b21965060f0
sorrer <sorrer@gmail.com>
2022-03-01 16:03:54 -0500

    Fixed Network Stats invalid logic

--- Assets/Scripts/Game/Common/UI/DebugUI/NetworkStatsUI.cs
+++ Assets/Scripts/Game/Common/UI/DebugUI/NetworkStatsUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5390fd1ef0834318a7f85409a4a6d14bf42c5d32d897fc10e428bea6ce412553
-size 3530
+oid sha256:d6d7056f644b0c07ae42bc80c609d80ed48acd329cfe4b2398d5f2c790c7c2c8
+size 3563
