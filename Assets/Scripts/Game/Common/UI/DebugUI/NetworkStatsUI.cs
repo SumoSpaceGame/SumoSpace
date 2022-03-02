b17a5f67e5527997fa0b8ff8e3c4867176ffbe7d
sorrer <sorrer@gmail.com>
2022-03-01 19:27:23 -0500

    Change IP to new Test Server + added server debug message

--- Assets/Scripts/Game/Common/UI/DebugUI/NetworkStatsUI.cs
+++ Assets/Scripts/Game/Common/UI/DebugUI/NetworkStatsUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cb0c27bc4d1b84f1167f02709132f78db038fb28b99da2e99255f5f9b3ef8b6
-size 4319
+oid sha256:e43229592eaee0a3fa9997858363a36f81616654e7a1f96cdd70e24b762c5222
+size 4613
