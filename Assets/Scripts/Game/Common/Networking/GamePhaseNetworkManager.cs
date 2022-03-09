bccd572d9e6d0d3524ec86cec5cb106ed5e994cd
sorrer <sorrer@gmail.com>
2022-03-08 20:03:08 -0500

    Debug Web Server hosting created, implementation required

--- Assets/Scripts/Game/Common/Networking/GamePhaseNetworkManager.cs
+++ Assets/Scripts/Game/Common/Networking/GamePhaseNetworkManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1cc4903b17c78367a65fb943a1f389011db105633512f3f4534207ca9d901f2a
-size 7004
+oid sha256:0ac6a860d51dba670ce7573dc880cfbce434a432ce3c0fde5ba5fd847cd9050d
+size 7008
--- Assets/Scripts/Game/Server/DebugServer/DebugWebserver.cs
+++ Assets/Scripts/Game/Server/DebugServer/DebugWebserver.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:173d33ae1de9287d335d3542d9b91b6e83b2b9179f5a4150e09c1d4f1eba28ed
-size 207
+oid sha256:dee0467caf4a666e838b6b4c8af236727d191e4faef6f662830ab95c3082892e
+size 3791
