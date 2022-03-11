59262956b6bc20cc02c7f7d3f3e75a312f5860a6
Luke Letourneau <LukeLetourneau2000@gmail.com>
2022-03-11 16:37:45 -0500

    Unversioned files oops

---
+++ Assets/Scripts/Game/Common/Gameplay/Abilities/Heavy/HeavyPrimaryFireAbility.cs
@@ -0,0 +1,18 @@
+using Game.Common.Gameplay.Ship;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Ship Abilities/Heavy Abilities/Heavy Primary Fire", fileName = "Heavy Primary Fire", order = 1)]
+public class HeavyPrimaryFireAbility : ShipAbility {
+
+    [SerializeField] private float minKnockback;
+    public float MinKnockback => minKnockback;
+
+    [SerializeField] private float maxKnockback;
+    public float MaxKnockback => maxKnockback;
+
+    [SerializeField] private float rampTime;
+    public float RampTime => rampTime;
+    
+    [SerializeField] private AnimationCurve knockbackCurve;
+    public AnimationCurve KnockbackCurve => knockbackCurve;
+}
