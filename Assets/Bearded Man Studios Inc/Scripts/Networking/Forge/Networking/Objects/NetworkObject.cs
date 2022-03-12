3f14d54521b8c166781ebc7461f5080aa7c3c0d3
sorrer <sorrer@gmail.com>
2022-03-12 17:18:23 -0500

    Added condition to check for allowing replication if dirty field is sent

--- Assets/Bearded Man Studios Inc/Scripts/Networking/Forge/Networking/Objects/NetworkObject.cs
+++ Assets/Bearded Man Studios Inc/Scripts/Networking/Forge/Networking/Objects/NetworkObject.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42ba8811a1fb73f91bdb63ce42774a9c6a453331e7de796a99ca5da9883f5f4a
-size 59370
+oid sha256:ea5455da698ee8890eb91c82c631d1b2184a1aa4a5fc512b7101f700f9020d82
+size 59595
