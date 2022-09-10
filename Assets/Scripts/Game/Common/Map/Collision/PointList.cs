df2a55eeb25f681808767f08f4500bd3f5952d76
sorrer <sorrer@gmail.com>
2022-09-10 13:46:42 -0400

    Added ray crossing for polygon inside detection (untested)

---
+++ Assets/Scripts/Game/Common/Map/Collision/PointList.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d0ea10f5518d4361d96e12e3387014a6d27bfba10660f69058e02c1a982bd7e4
+size 293
---
+++ Assets/Scripts/Game/Common/Map/Collision/RayCrossing.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f306e9f29b47d16d09614e1565c494c3c14a923128b407cf80ff548cff31eaf1
+size 2407
