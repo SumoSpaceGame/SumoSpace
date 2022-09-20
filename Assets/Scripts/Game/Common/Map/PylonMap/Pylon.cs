04984796be2babec961e0104652920420deaf1ab
sorrer <sorrer@gmail.com>
2022-09-19 21:35:26 -0400

    Cleaned up some debug + added more testing inside the map scene

--- Assets/Scripts/Game/Common/Map/PylonMap/Pylon.cs
+++ Assets/Scripts/Game/Common/Map/PylonMap/Pylon.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2819677e8782d8c68970c243a638f98afd1d82f5e7c069efbba72594806e82c6
-size 2955
+oid sha256:d0e2a953c51c48c5647d099a9ed5f9cfe787734e4a193e2debbd31d84f69df6c
+size 3021
--- Assets/Scripts/Game/Common/Map/TestWithin.cs
+++ Assets/Scripts/Game/Common/Map/TestWithin.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3e67b9e884a61e5c9c0b766ef31b76b9a1917cb1a1214c75d3d84c5dd59649f
+oid sha256:5f9de11d72f92c9abe7907d74cd24619ac7ebcac5d718942fedf4b377aeb8b17
 size 959
