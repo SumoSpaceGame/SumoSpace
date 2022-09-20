a4975a86afdf25772253eccad3f15d31088d3b9e
sorrer <sorrer@gmail.com>
2022-09-19 21:16:16 -0400

    Project upgraded to 2021.3.10f lts

--- Assets/Scripts/Game/Client/Input/Controls.cs
+++ Assets/Scripts/Game/Client/Input/Controls.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea2c8c910ef7e9fef1acdca7d7c43890f6930fcc50d1aca42ceff03d7e8c9292
+oid sha256:36e683d8ef51d6b77bf3795bfddd171dcc9d0ace9262acdad5f4b38b224c1490
 size 16429
