5bb830cdc94e02c6c90b312e24f2e23ce4bced1a
sorrer <sorrer@gmail.com>
2022-09-20 00:24:35 -0400

    Added null check for pylon graphics

--- Assets/Scripts/Game/Common/Map/PylonMap/Pylon.cs
+++ Assets/Scripts/Game/Common/Map/PylonMap/Pylon.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db7316f203923e4e76b2f568ae9d0c349ea4747cb6f4d970966d2c74f1aa87f9
-size 3152
+oid sha256:f62e734603e05e848a576aa764015ce062fe763e42b80a0fcd3b6b014085b6fd
+size 3214
