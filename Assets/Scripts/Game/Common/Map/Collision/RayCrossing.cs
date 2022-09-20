fc23732bbf951332738b427dcbcbc5cc27dec950
sorrer <sorrer@gmail.com>
2022-09-19 21:20:02 -0400

    More testing + increased ray crossing range

--- Assets/Scripts/Game/Common/Map/Collision/RayCrossing.cs
+++ Assets/Scripts/Game/Common/Map/Collision/RayCrossing.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b03538263a687eee931832681c3a444fd8719d1d1b63bc66ebad440cd99519de
-size 4845
+oid sha256:4e16d956fa100dd41fa3e67419c9b7b653516b60f4eedc6d9d348c93951625a0
+size 4847
