45d02f889035835f1ddf138be129643d415bbdca
sorrer <sorrer@gmail.com>
2022-09-23 23:16:28 -0400

    Added debug client to tool

--- Assets/Scripts/Editor/BuildingTools/EditorBuildTools.cs
+++ Assets/Scripts/Editor/BuildingTools/EditorBuildTools.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7def0ea41cf5fc8e1b2798c69b392803ca587b1bbcc3bcc58736574505aa94d1
-size 4584
+oid sha256:bfe32bfafdaecd838cc84dd8fab67c3e57ea274b4cf47efabff551903e051669
+size 4967
