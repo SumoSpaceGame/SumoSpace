2af811bdfef31f205c8eb19ac2f9cbb88c1e0e0a
sorrer <sorrer@gmail.com>
2022-09-24 00:23:50 -0400

    Made more complex test map

--- Assets/Scripts/Game/Common/Map/PylonMap/PylonMap.cs
+++ Assets/Scripts/Game/Common/Map/PylonMap/PylonMap.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78e8fccdeb3d96c99e4959201c7712446f896322ec1cad3b10cb123c34ab0457
-size 4009
+oid sha256:a3cb119970e9038e850575d15dddfdcb30e67e217514349f18946d3d8932720d
+size 4095
