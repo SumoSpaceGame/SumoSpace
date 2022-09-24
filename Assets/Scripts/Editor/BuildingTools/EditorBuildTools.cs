59ca19c6204566a7d62f627220c7eb7754bc540c
sorrer <sorrer@gmail.com>
2022-09-23 23:14:52 -0400

    Added debug server option to build tool. Also added ability to toggle Init server bool without having to go to the object

--- Assets/Scripts/Editor/BuildingTools/EditorBuildTools.cs
+++ Assets/Scripts/Editor/BuildingTools/EditorBuildTools.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:682d8572f1e7d98b75b358701e37c2521cb81823e022710ab94f9ede03a07e8a
-size 3784
+oid sha256:7def0ea41cf5fc8e1b2798c69b392803ca587b1bbcc3bcc58736574505aa94d1
+size 4584
