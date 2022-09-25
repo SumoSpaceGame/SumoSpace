525a99d1f7b39f688b5ce126adb4bee62909d1ff
sorrer <sorrer@gmail.com>
2022-09-25 01:45:42 -0400

    Added try get value

--- Assets/Scripts/Game/Common/Instances/MainPersistantInstances.cs
+++ Assets/Scripts/Game/Common/Instances/MainPersistantInstances.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39646570fe720fd71a48b238ce6dac31bb5d930b46963c719783bc90525b8d55
-size 3418
+oid sha256:e5336993f6ab26a2dcdb61789c683f498b1f5cd3e7f4dee61fc4eefa98b1cb95
+size 3786
