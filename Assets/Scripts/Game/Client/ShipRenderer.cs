5f66fe31bf4d6c83fd9cce8b116cd8ba318ba2e1
sorrer <sorrer@gmail.com>
2022-09-25 22:17:26 -0400

    Disabled temp tracer beam

--- Assets/Scripts/Game/Client/ShipRenderer.cs
+++ Assets/Scripts/Game/Client/ShipRenderer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffe2639e767bdc629433da5476b329fcdba91066246357f483ea82e8f2f1f77f
-size 1056
+oid sha256:b3706f2a4e0369f7cf84507b0d0d87080b11a1946db9944c7ffbe11779841f4d
+size 1058
