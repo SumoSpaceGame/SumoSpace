7488a5e840d5a21024ad67d06284af330a0f34cf
sorrer <sorrer@gmail.com>
2022-09-27 02:02:55 -0400

    Fixed client not handling phase update RPCs

--- Assets/Scripts/Game/Common/Networking/GamePhaseNetworkManager.cs
+++ Assets/Scripts/Game/Common/Networking/GamePhaseNetworkManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5108183ead296acb69d3946748b844e91f51d5387522e7df1ce7ce1c396d4e53
-size 6368
+oid sha256:9e178c23a15aa472ab9f95023e6083ca39da2f9c802e8f12e5b614833a3d5900
+size 7066
