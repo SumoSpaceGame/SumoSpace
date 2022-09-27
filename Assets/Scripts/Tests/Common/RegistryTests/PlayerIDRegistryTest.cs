c5f2231cb8291290008850efba02872f8c10215e
sorrer <sorrer@gmail.com>
2022-09-27 01:35:35 -0400

    Updated test cases

--- Assets/Scripts/Tests/Common/RegistryTests/PlayerIDRegistryTest.cs
+++ Assets/Scripts/Tests/Common/RegistryTests/PlayerIDRegistryTest.cs
@@ -17,28 +17,28 @@ namespace Tests.RegistryTests
 
             PlayerID tempData;
             Assert.IsFalse(instance.TryGetByNetworkID(0, out tempData));
-            Assert.IsFalse(instance.TryGetByNetworkID(uint.MaxValue/2, out tempData));
-            Assert.IsFalse(instance.TryGetByNetworkID(uint.MaxValue, out tempData));
+            Assert.IsFalse(instance.TryGetByNetworkID(int.MaxValue/2, out tempData));
+            Assert.IsFalse(instance.TryGetByNetworkID(int.MaxValue, out tempData));
         
         
             Assert.IsTrue(instance.RegisterPlayer(0, 1));
-            Assert.IsTrue(instance.RegisterPlayer(uint.MaxValue/2, 2));
-            Assert.IsTrue(instance.RegisterPlayer(uint.MaxValue, 3));
+            Assert.IsTrue(instance.RegisterPlayer(int.MaxValue/2, 2));
+            Assert.IsTrue(instance.RegisterPlayer(int.MaxValue, 3));
             
             Assert.IsFalse(instance.RegisterPlayer(0, 1));
-            Assert.IsFalse(instance.RegisterPlayer(uint.MaxValue/2, 2));
-            Assert.IsFalse(instance.RegisterPlayer(uint.MaxValue, 3));
+            Assert.IsFalse(instance.RegisterPlayer(int.MaxValue/2, 2));
+            Assert.IsFalse(instance.RegisterPlayer(int.MaxValue, 3));
         
         
             Assert.IsTrue(instance.TryGetByNetworkID(0, out tempData));
-            Assert.IsTrue(instance.TryGetByNetworkID(uint.MaxValue/2, out tempData));
-            Assert.IsTrue(instance.TryGetByNetworkID(uint.MaxValue, out tempData));
+            Assert.IsTrue(instance.TryGetByNetworkID(int.MaxValue/2, out tempData));
+            Assert.IsTrue(instance.TryGetByNetworkID(int.MaxValue, out tempData));
         
             instance.Reset();
 
             Assert.IsFalse(instance.TryGetByNetworkID(0, out tempData));
-            Assert.IsFalse(instance.TryGetByNetworkID(uint.MaxValue/2, out tempData));
-            Assert.IsFalse(instance.TryGetByNetworkID(uint.MaxValue, out tempData));
+            Assert.IsFalse(instance.TryGetByNetworkID(int.MaxValue/2, out tempData));
+            Assert.IsFalse(instance.TryGetByNetworkID(int.MaxValue, out tempData));
         }
 
     }
