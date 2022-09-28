9b13037d4c4914fa155eaa4558f895e17ecfe463
sorrer <sorrer@gmail.com>
2022-09-28 00:51:04 -0400

    Add temp solution for double connects

--- Assets/Scripts/Game/Common/Networking/FishNetConnector.cs
+++ Assets/Scripts/Game/Common/Networking/FishNetConnector.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5471e9b5d10fb5695c738ee5bedeeae93673f96a4195ed1690c00466ed0060e4
-size 2170
+oid sha256:f31eb7077e222bcdf68ad44f43a1bd16cbec6cfe363fdbed7b140d5a1ca8002c
+size 2294
