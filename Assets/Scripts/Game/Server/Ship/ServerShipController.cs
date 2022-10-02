270c26240d196de320372eebd994acf6cd4dfb76
sorrer <sorrer@gmail.com>
2022-10-02 15:51:04 -0400

    Added rigidbody reset for teleport

--- Assets/Scripts/Game/Server/Ship/ServerShipController.cs
+++ Assets/Scripts/Game/Server/Ship/ServerShipController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:febc151141e33316cc4128d486097caef04ad5cd37f26241b9fc31aac0685353
-size 6248
+oid sha256:7e3ee4debe4cfb445cf44f9ec986e48e70531238881de19792e1b27ed8aff099
+size 6311
