051224af42eb365e7222203a5e0a16c62cb032e2
sorrer <sorrer@gmail.com>
2022-10-02 13:30:17 -0400

    Added reset inertia

--- Assets/Scripts/Game/Common/Gameplay/Ship/ShipController.cs
+++ Assets/Scripts/Game/Common/Gameplay/Ship/ShipController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f82d9554a04280fa733efdc8abb25e2db3fd9202b67aae6503d8d913d826d09c
-size 659
+oid sha256:d5138622a7996332aa3b152618d165a6aeafdd650c3411c4df7022fbc681fccf
+size 705
--- Assets/Scripts/Game/Server/Ship/ServerShipController.cs
+++ Assets/Scripts/Game/Server/Ship/ServerShipController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ff08fc75f7647d7b2fdf1752e13156bbb3394285bd89250152d21d1c22fa74a
-size 5952
+oid sha256:febc151141e33316cc4128d486097caef04ad5cd37f26241b9fc31aac0685353
+size 6248
