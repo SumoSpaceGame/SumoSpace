a5413998bfd581c455c1f540b5effeac98610d8e
sorrer <sorrer@gmail.com>
2022-10-02 13:36:29 -0400

    Added a way to teleport a ship

--- Assets/Scripts/Game/Common/Gameplay/Ship/ShipManager.cs
+++ Assets/Scripts/Game/Common/Gameplay/Ship/ShipManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8aa0991a607aa4f6bdb35275177f9ab24b98c4a55ccfc6b348a129ac0e17be12
-size 3364
+oid sha256:0c2eef3cf84b0b9a097121b9449058421a22ee98c0e85baa98c661cc9b8e0250
+size 4068
