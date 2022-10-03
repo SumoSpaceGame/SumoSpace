40e01713128f6c12699e2af18a5ec071396f5b23
sorrer <sorrer@gmail.com>
2022-10-02 20:41:52 -0400

    Added port to parameter options

--- Assets/Scripts/Game/Common/Settings/MasterSettings.cs
+++ Assets/Scripts/Game/Common/Settings/MasterSettings.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:adf9fa260858a46187d8d8e2916c4cc5aaa139a03b88ab477c0a32b1488a6b37
-size 5255
+oid sha256:af072f1776dbff2e1b5d217ccb55f50c34210299125d4255b121c1831a2a9829
+size 5384
