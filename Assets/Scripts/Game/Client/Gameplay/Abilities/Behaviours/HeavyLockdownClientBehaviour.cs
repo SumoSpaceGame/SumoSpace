e44711ca7e076afe3555f4ea5966b148771145d0
averageJoe325 <54290919+averageJoe325@users.noreply.github.com>
2022-10-22 21:50:47 -0400

    Fixed some backs
    Bugs Fixed:
    Fixed the laser behaving weird when the mouse is being held during windup/winddown.
    Fixed the ships jittering around for no reason.
    Fixed timer incrementing during windup/winddown

--- Assets/Scripts/Game/Client/Gameplay/Abilities/Behaviours/HeavyLockdownClientBehaviour.cs
+++ Assets/Scripts/Game/Client/Gameplay/Abilities/Behaviours/HeavyLockdownClientBehaviour.cs
@@ -3,14 +3,53 @@ using UnityEngine;
 
 public class HeavyLockdownClientBehaviour : RenderableAbilityBehaviour<HeavyLockdownAbility>
 {
+    private HeavyPrimaryFireAbility heavyFire = null;
+
     public override void Execute()
     {
-        
+        if (heavyFire is null)
+        {
+            heavyFire = shipManager.shipLoadout.PrimaryFire as HeavyPrimaryFireAbility;
+            if (heavyFire is null)
+            {
+                Debug.LogError("No Heavy Primary Fire Ability found.");
+                return;
+            }
+        }
+        if (heavyFire.IsDisabled)
+            return;
+        if (!executing)
+        {
+            shipManager.StartCoroutine(ClientSideStart());
+            executing = true;
+        }
+        else
+        {
+            shipManager.StartCoroutine(ClientSideStop());
+            executing = false;
+        }
     }
 
-    public override void QuickExecute()
+    // Winds up and activates the lockdown.
+    private IEnumerator ClientSideStart()
     {
-        Debug.Log("Starting Lockdown on client.");
+        heavyFire.IsDisabled = true;
+        yield return new WaitForSeconds(Ability.WindUpTime);
+        heavyFire.IsDisabled = false;
+        yield return null;
+    }
+
+    // Winds down and deactivates the lockdown.
+    private IEnumerator ClientSideStop()
+    {
+        heavyFire.IsDisabled = true;
+        yield return new WaitForSeconds(Ability.WindDownTime);
+        heavyFire.IsDisabled = false;
+        yield return null;
+    }
+
+    /*public override void QuickExecute()
+    {
         if (++oooCounter == 1)
         {
             executing = true;
@@ -23,5 +62,5 @@ public class HeavyLockdownClientBehaviour : RenderableAbilityBehaviour<HeavyLock
         {
             executing = false;
         }
-    }
+    }*/
 }
