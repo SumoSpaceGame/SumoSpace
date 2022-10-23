997c44f2bef312f00ce3767fd2ec807174364c0c
averageJoe325 <54290919+averageJoe325@users.noreply.github.com>
2022-10-23 18:06:30 -0400

    Added burst effect
    New features:
    Heavy ship's secondary ability, burst, has been implemented. It knocks away all ships within a certain radius.
    
    To do:
    Still needs a charge meter, charge is treated as always full.

---
+++ Assets/Scripts/Game/Server/Abilities/Behaviours/HeavyBurstServerBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using Game.Common.Gameplay.Ship;
+using Game.Common.Instances;
+using Game.Common.Networking;
+using Game.Common.Registry;
+using Game.Common.Settings;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.HID;
+
+public class HeavyBurstServerBehaviour : AbilityBehaviour<HeavyBurstAbility>
+{
+    // Placeholder charge until the bar system is implemented.
+    private float charge = 1;
+
+    // Apply force to every ship in the radius.
+    public override void Execute()
+    {
+        if (charge < Ability.MinCharge)
+            return;
+        Dictionary<PlayerID, ShipManager>.ValueCollection ships = MainPersistantInstances.Get<GameNetworkManager>().masterSettings.playerShips.GetAll();
+        foreach (ShipManager ship in ships)
+        {
+            if (shipManager == ship)
+                continue;
+            Vector2 shipManagerToShip = ship.Position - shipManager.Position;
+            float distance = shipManagerToShip.magnitude;
+            float impulse = Ability.Impluse(charge, distance);
+            if (impulse != 0)
+                ship._rigidbody2D.AddForce(shipManagerToShip.normalized * impulse, ForceMode2D.Impulse);
+        }
+        // TODO: Remove charge used.
+    }
+}
