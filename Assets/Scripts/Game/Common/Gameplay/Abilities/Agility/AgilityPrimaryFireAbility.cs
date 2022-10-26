88a475f9f4ec197a87b53fa052cfc76870111231
Luke Letourneau <Bluestackthingie@gmail.com>
2022-10-25 20:36:45 -0400

    New art assets + Project structure

--- Assets/Scripts/Game/Common/Gameplay/Abilities/Agility/AgilityPrimaryFireAbility.cs
+++
@@ -1,9 +0,0 @@
-using Game.Common.Gameplay.Abilities;
-using UnityEngine;
-
-[CreateAssetMenu(menuName="Ship Abilities/Agility Abilities/Agility Primary Fire", fileName = "Agility Primary Fire", order=1)]
-public class AgilityPrimaryFireAbility: ShipAbility {
-
-    [SerializeField] private float knockback;
-    public float Knockback => knockback;
-}
